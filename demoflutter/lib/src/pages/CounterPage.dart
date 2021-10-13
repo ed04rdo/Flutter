82405756f4beeae386089467b960b6c2b99a45d0
ed04rdo <eduardo.ra.04@gmail.com>
2021-10-13 17:09:34 -0500

    Substract & reset functionality added

--- demoflutter/lib/src/pages/CounterPage.dart
+++ demoflutter/lib/src/pages/CounterPage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, use_key_in_widget_constructors
+// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -15,7 +15,7 @@ class _CounterPageState extends State<CounterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Statefuk'),
+        title: const Text('Stateful'),
         centerTitle: true,
         foregroundColor: Colors.white,
       ),
@@ -28,12 +28,41 @@ class _CounterPageState extends State<CounterPage> {
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            _contador++;
-          }),
+      floatingActionButton: _crearBotones(),
     );
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      children: <Widget>[
+        SizedBox(width: 30.0),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_zero),
+          onPressed: () => _reset(),
+        ),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(
+          child: const Icon(Icons.remove),
+          onPressed: () => _sub(),
+        ),
+        SizedBox(width: 10.0),
+        FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => _add(),
+        )
+      ],
+    );
+  }
+
+  void _reset() {
+    setState(() => _contador = 0);
+  }
+
+  void _sub() {
+    setState(() => _contador--);
+  }
+
+  void _add() {
+    setState(() => _contador++);
   }
 }
